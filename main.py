055bc7ba92f5d549916e1c6567bf2e782d5e108a
Nox <vanny03@mail.ru>
2023-04-24 22:53:05 +0300

    Первый коммит, начал создавать ботта, пока он умеет отправлять фото котиков :)

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+import time
+
+
+API_URL: str = 'https://api.telegram.org/bot'
+BOT_TOKEN: str = '6050230331:AAHHUbh4mfaIsRbWm0p6jZdLTUIUQl4O_uA'
+TEXT: str = 'Ура! Классный апдейт!'
+API_CATS_URL: str = 'https://api.thecatapi.com/v1/images/search'
+MAX_COUNTER: int = 100
+ERROR_TEXT: str = 'Здесь должна была быть картинка с котиком :('
+
+offset: int = -2
+counter: int = 0
+chat_id: int
+cat_response: requests.Response
+cat_link: str
+
+while True:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+
+    time.sleep(1)
+    counter += 1
