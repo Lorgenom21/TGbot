36fd3d3216c1ef9548b678a3baee5041131bac42
Nox <vanny03@mail.ru>
2023-04-26 22:47:23 +0300

    Полностью доделал Эхо-бота

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.types import ContentType
+from aiogram import F
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
 # полученный у @BotFather
@@ -21,16 +23,20 @@ async def process_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь и в ответ '
                          'я пришлю тебе твое сообщение')
 
-
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
-async def send_echo(message: Message):
-    await message.answer(text=message.text)
+async def send_echo(message : Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+        print(message.json(indent = 4, exclude_none= True))
+    except TypeError:
+        await message.reply(text="Данный тип не поддерживается методом send_copy")
 
 
 # Регистрируем хэндлеры
 dp.message.register(process_start_command, Command(commands=["start"]))
 dp.message.register(process_help_command, Command(commands=['help']))
+
 dp.message.register(send_echo)
 
 if __name__ == '__main__':
